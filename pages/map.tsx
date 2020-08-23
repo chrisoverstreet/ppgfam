ef2ca4e2db28ba7e3a910e3d20311f50801d01aa
chrisoverstreet <christopheroverstreet@gmail.com>
2020-08-23 00:11:07 -0400

    Add bounds to map

--- pages/map.tsx
+++ pages/map.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import ReactMapGL, { Marker, ViewportProps } from 'react-map-gl';
 
 import MainLayout from '../components/MainLayout';
+import { Coordinates } from 'viewport-mercator-project';
 import { useLocationsQuery } from '../__generated__/graphql';
 import { useRouter } from 'next/router';
+import { WebMercatorViewport } from 'react-map-gl';
 
 const Map: React.FunctionComponent = () => {
   const router = useRouter();
@@ -11,9 +13,7 @@ const Map: React.FunctionComponent = () => {
   const query: string =
     (Array.isArray(router.query.q) ? router.query.q[0] : router.query.q) || '';
 
-  const [viewport, setViewport] = React.useState<
-    Pick<ViewportProps, 'latitude' | 'longitude' | 'zoom'>
-  >({
+  const [viewport, setViewport] = React.useState<Partial<ViewportProps>>({
     latitude: 37.3343,
     longitude: -79.5231,
     zoom: 4,
@@ -22,6 +22,40 @@ const Map: React.FunctionComponent = () => {
   const { data } = useLocationsQuery({
     variables: { query },
   });
+
+  React.useEffect(() => {
+    const bounds: [Coordinates, Coordinates] = (
+      data?.locations.edges ?? []
+    ).reduce(
+      (
+        [[westLongitude, southLatitude], [eastLongitude, northLatitude]],
+        { node: { longitude, latitude } },
+      ) => [
+        [Math.min(westLongitude, longitude), Math.min(southLatitude, latitude)],
+        [Math.max(eastLongitude, longitude), Math.max(northLatitude, latitude)],
+      ],
+      [
+        [
+          data?.locations.edges[0]?.node.longitude ?? -79.5231,
+          data?.locations.edges[0]?.node.latitude ?? 37.3343,
+        ],
+        [
+          data?.locations.edges[0]?.node.longitude ?? -79.5231,
+          data?.locations.edges[0]?.node.latitude ?? 37.3343,
+        ],
+      ],
+    );
+
+    const newViewport = new WebMercatorViewport({
+      height: viewport.height || 500,
+      width: viewport.width || 500,
+    }).fitBounds(bounds, { padding: 25 });
+
+    setViewport({
+      ...newViewport,
+      zoom: Math.min(newViewport.zoom, 8),
+    });
+  }, [data]);
 
   return (
     <MainLayout>
